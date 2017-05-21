bc97c5c6628bffed0c92afe8ab588137a4c380b6
prog470dev <shimizutaiki@shimizudaiki-no-MacBook-Pro.local>
2017-05-22 00:05:53 +0900

    update

--- icpc/railway_connection.cpp
+++ icpc/railway_connection.cpp
@@ -26,7 +26,7 @@ using namespace std;
 int n,m,c,s,g;
 int x,y,d,comp,p[25],q,r[25][55];
 vector<pair<int,pair<int,int> > > rail[1010];  //駅、距離、会社
-int dp[110][25][2000010];  //スタート(s-1,0)から状態（駅番号[0->n-1]、直前に使った駅[1!!->c]）までの最小コスト
+int dp[110][25];  //スタート(s-1,0)から状態（駅番号[0->n-1]、直前に使った駅[1!!->c]）までの最小コスト
                            //直前の会社=0 は、スタートの時のみ（駅は１始まりインデックスを最小）
 vector<pair<pair<int,int>,int > > compCost[25];//<<距離区間>,r>
 
@@ -36,11 +36,11 @@ int main() {
     if(n==0&&m==0&&c==0&&s==0&&g==0) break;
     REP(i,0,m){
       cin>>x>>y>>d>>comp;
-      rail[x-1].push_back(make_pair(y-1, make_pair(d,comp)));
-      rail[y-1].push_back(make_pair(x-1, make_pair(d,comp)));
+      rail[x-1].push_back(make_pair(y-1, make_pair(d,comp-1)));
+      rail[y-1].push_back(make_pair(x-1, make_pair(d,comp-1)));
     }
-    REP(i,1,c+1) cin>>p[i];
-    REP(i,1,c+1){
+    REP(i,0,c) cin>>p[i];
+    REP(i,0,c){
       compCost[i].push_back(make_pair(make_pair(0,INF),INF));
       REP(j,0,p[i]-1){
         cin>>q;
@@ -54,69 +54,62 @@ int main() {
       }
     }
 
-    REP(i,0,n)REP(j,0,c)REP(k,0,2000010) dp[i][j][k] = INF;
+    REP(i,0,n)REP(j,0,c) dp[i][j] = INF;
 
     //<<距離>,<駅,<直前の会社,"その会社で"走ってきた距離>>>
     priority_queue< pair< int, pair<int,pair<int,int> > > > PQ;
     PQ.push(make_pair(0, make_pair(s-1,make_pair(0,0))));
-    dp[s-1][0][0] = 0;
+    REP(i,0,c) dp[s-1][i] = 0;
 
     while(!PQ.empty()){
       int nowNode = PQ.top().S.F;
       int nowComp = PQ.top().S.S.F;
       int nowDist = PQ.top().S.S.S; //会社nowCompで走ってきた距離
-      int nowCost = PQ.top().F;
+      int nowCost = (-1)*PQ.top().F;
       PQ.pop();
-      if(dp[nowNode][nowComp][nowDist] < nowCost) continue;
+      if(dp[nowNode][nowComp] < nowCost) continue;
 
-      cout<<"nowNode:"<<nowNode<<endl;
+      cout<<"nowNode:"<<nowNode<<"(nowCost:"<<nowCost<<")"<<endl;
 
       REP(i,0,rail[nowNode].size()){
         int nextNode = rail[nowNode][i].F;
         int nextComp = rail[nowNode][i].S.S;
 
-        int nextDist;// = rail[nowNode][i].S.F;
+        int nextDist; //"nextComp"で走る距離(個々まで距離を含める場合もある)
         if(nowComp==nextComp) nextDist = nowDist + rail[nowNode][i].S.F;
         else nextDist = rail[nowNode][i].S.F;
 
-        int tmpCost=0;//直前までの会社で走った距離にかかったコスト
-        REP(i,0,compCost[nowComp].size()){
-          if(compCost[nowComp][i].F.F <= nowDist && nowDist <= compCost[nowComp][i].F.S){
-            if(i>0) tmpCost = compCost[nowComp][i-1].S + r[nowComp][i]*(nowDist- compCost[nowComp][i].F.F);
-            else tmpCost = r[nowComp][i]*(nowDist- compCost[nowComp][i].F.F);
-            break;
-          }
-        }
+        cout<<"---nextNode : "<<nextNode<<", nextDist : "<<nextDist<<"("<<rail[nowNode][i].S.F<<")"<<endl;
 
         int nextCost=0; //線路がつながっているから、何かしらの値は必ずある
-        REP(i,0,compCost[nextComp].size()){
-          if(compCost[nextComp][i].F.F <= nextDist && nextDist <= compCost[nextComp][i].F.S){
-            if(i>0) nextCost = compCost[nextComp][i-1].S + r[nextComp][i]*(nextDist- compCost[nextComp][i].F.F);
-            else nextCost = r[nextComp][i]*(nextDist- compCost[nextComp][i].F.F);
-            if(nowComp!=nextCost)cout<<">> nextNode:"<<nextNode<<", nextDist:"<<nextDist<<", nextCost"<<nextCost<<endl;
-            break;
-          }
+        REP(j,0,compCost[nextComp].size()){
+          if(nextDist <= compCost[nextComp][j].F.F) break;
+          if(j<compCost[nextComp].size()-1) nextCost += r[nextComp][j]*min(nextDist-compCost[nextComp][j].F.F, compCost[nextComp][j].F.S-compCost[nextComp][j].F.F);
+          else nextCost += r[nextComp][j]*(nextDist-compCost[nextComp][j].F.F);
+          cout<<"------>>>"<<nextDist-compCost[nextComp][j].F.F<<", "<<compCost[nextComp][j].F.S-compCost[nextComp][j].F.F<<", r:"<<r[nextComp][j]<<endl;
         }
+        int tmpCost=0; //重複を差し引くため
+        REP(j,0,compCost[nextComp].size()){
+          if(nowDist <= compCost[nextComp][j].F.F) break;
+          if(j<compCost[nextComp].size()-1) tmpCost += r[nextComp][j]*min(nowDist-compCost[nextComp][j].F.F, compCost[nextComp][j].F.S-compCost[nextComp][j].F.F);
+          else tmpCost += r[nextComp][j]*(nowDist-compCost[nextComp][j].F.F);
+        }
+        if(nowComp==nextComp) nextCost -= tmpCost;
 
-        if(nowComp==nextComp){
-          if(nowCost+nextCost-tmpCost < dp[nextNode][nextComp][nextDist]){
-            dp[nextNode][nextComp][nextDist] = nowCost+nextCost-tmpCost;  //nextConsrに以前の走行のコストも入れてしまっている
-            //cout<<"--- nextNode:"<<nextNode<<", dp[nextNode][nextComp][nextDist]:"<<dp[nextNode][nextComp][nextDist]<<endl;
-            PQ.push(make_pair(dp[nextNode][nextComp][nextDist], make_pair(nextNode, make_pair(nextComp,nextDist))));
-          }
-        }else{
-          if(nowCost+nextCost < dp[nextNode][nextComp][nextDist]){
-            dp[nextNode][nextComp][nextDist] = nowCost+nextCost;
-            //cout<<"--- nextNode:"<<nextNode<<", dp[nextNode][nextComp][nextDist]:"<<dp[nextNode][nextComp][nextDist]<<endl;
-            PQ.push(make_pair(dp[nextNode][nextComp][nextDist], make_pair(nextNode, make_pair(nextComp,nextDist))));
-          }
+        if(nowComp==nextComp)cout<<"(tmpCost:"<<tmpCost<<")"<<endl;
+        cout<<"------nextCost: "<<nextCost<<", dp[nextNode][nextComp]: "<<dp[nextNode][nextComp]<<endl;
+
+        if(nowCost+nextCost < dp[nextNode][nextComp]){
+          dp[nextNode][nextComp] = nowCost+nextCost;
+          PQ.push(make_pair((-1)*dp[nextNode][nextComp], make_pair(nextNode, make_pair(nextComp,nextDist))));
+          cout<<"PUSH!!"<<endl;
         }
 
       }
     }
 
     int mincost = INF;
-    REP(i,1,c)REP(j,0,2000010) mincost = dp[g-1][i][j];
+    REP(j,0,c) mincost = min(mincost,dp[g-1][j]);
 
     if(mincost==INF) cout<<-1<<endl;
     else cout<<mincost<<endl;
