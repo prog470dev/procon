9368f03e8e121b5de00b8a9a19c6d7e0ccae8618
西尾大智 <dnishio@dnishio-MBP.csl.ec.t.kanazawa-u.ac.jp>
2018-03-26 19:02:45 +0900

    modify Nibutan.cpp and Sanbutan.cpp

--- library/Explore/sanbutan.cpp
+++ library/Explore/sanbutan.cpp
@@ -1,41 +1,57 @@
 /*
 三分探索
 なにかが凸関数で表せる場合に使う
-search(ll mid)に条件かくだけ
+絶対値をとる時などはあやしい(最小値よりはなれていくほど答えも値がはなれていく)
 最小値を求める時と最大値をもとめるときで、
 不等号が逆になるので注意
 
+例:
+ yukicoder No.198
+ https://yukicoder.me/problems/no/198
+ 箱の中身のキャンディーを同じ数にしたい
+
 */
 
+#include <algorithm>
+#include <iostream>
+typedef long long ll;
+using namespace std;
+const ll inf = 1e18;
 
+int N;
+ll B;
+ll C[11];
 ll search(ll mid){
-
+    ll tmp = 0;
+    ll a = B;
+    for(int i=0; i < N; i++) {
+        tmp += abs(mid - C[i]);
+        a += C[i] - mid;
+    }
+    if (a < 0) return inf;
+    return tmp;
 }
 
 int main() {
-	cin>>B>>N;
-	ll sum=B;
-	REP(i,0,N) {
-		cin>>C[i];
-		sum+=C[i];
-	}
-	if(N==1){
-		p(0);
-		return 0;;
-	}
-	sort(C,C+N);
-	ll lb=C[0];
-	ll ub=sum/N+1;
-	while(ub -lb > 3){
-		ll mid1=(lb * 2 + ub) / 3;
-		ll mid2=(lb + ub * 2) / 3;
-		if(search(mid1) > search(mid2)) lb=mid1;
-		else ub = mid2;
-	}
-	ll ans = inf;
-	REP(i,lb,ub){
-		ans=min(ans,search(i));
-	}
-	p(ans);
-	return 0;
+    cin>>B>>N;
+    ll sum=B;
+    for(int i=0; i<N; i++) {
+        cin>>C[i];
+        sum+=C[i];
+    }
+    sort(C,C+N);
+    ll lb=C[0];
+    ll ub=sum/N+1;
+    while(ub -lb > 3){
+        ll mid1=(lb * 2 + ub) / 3; //三等分して左を選ぶ
+        ll mid2=(lb + ub * 2) / 3; //三等分して右を選ぶ
+        if(search(mid1) > search(mid2)) lb=mid1; //左側の領域を捨てる
+        else ub = mid2;
+    }
+    ll ans = inf;
+    for(ll i=lb; i<ub; i++){//ギリギリのとこは絞れないので愚直に
+        ans=min(ans,search(i));
+    }
+    cout<<ans<<endl;
+    return 0;
 }
