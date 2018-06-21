dd30a2a8b82d5f55daaa25a2087a39be7e06ef17
西尾大智 <dnishio@dhcp218-65.wifi.kanazawa-u.ac.jp>
2018-06-21 12:22:56 +0900

    improve bellman_ford.cpp

--- library/MinimumPath/bellman_ford.cpp
+++ library/MinimumPath/bellman_ford.cpp
@@ -18,51 +18,83 @@
 #define REP(i, a, b) for (int i = a; i < b; i++)
 
 using namespace std;
+typedef long long ll;
 
-const int INF = 1e9;
-const int EDGE_SIZE = 10101;
-const int NODE_SIZE = 10101;
+const ll INF = 1e18;
 
-int N;  //グラフのノードの数
-int M;  //グラフの辺の数
-vector< pair<int, pair<int, int>> > edge[EDGE_SIZE];   //辺情報　<コスト, <formノード, toノード>>
-int d[NODE_SIZE];   //始点ノードからノードiへの最短距離
-int start;  //開始ノード番号
+const ll NODE_SIZE = 1010;
+ll N;  //グラフのノードの数
+ll M;  //グラフの辺の数
+vector< pair<ll, pair<ll, ll>> > edge;   //辺情報　<コスト, <formノード, toノード>>
+ll d[NODE_SIZE];   //始点ノードからノードiへの最短距離(score)
+ll start;  //開始ノード番号
 
-bool negCloCir = false; //負の閉路があるかどうかのフラグ
-
+bool negCloCir; //負の閉路があるかどうかのフラグ
+bool negNode[NODE_SIZE];
 /*==================================================*/
 
 //ベルマン・フォード法
-void bellman_ford() {
-  bool update;
-  REP(i,0,N-1){
-    update = false;
-    REP(j,0,edge.size()){
-      int from = edge[j].second.first;
-      int to = edge[j].second.second;
-      if(d[from] != INF && d[to] > d[from] + edge[j].F){
-        d[to] = d[from] + edge[j].F;
-        update = true;
-      }
+bool bellman_ford() {
+    bool update;
+    REP(i,0,N){
+        update = false;
+        for(auto e:edge){
+            ll from = e.second.first;
+            ll to = e.second.second;
+            if(d[from] != INF && d[to] > d[from] + e.first){
+                d[to] = d[from] + e.first;
+                update = true;
+
+            }
+        }
+        if(!update) break;
     }
-    if(!update) break;
-  }
-  if(update) negCloCir = true;  //負の閉路が存在
+    return update; //true: 負の閉路が存在
+}
+
+void check_negative() {
+    REP(i,0,N+1){
+        for(auto e:edge){
+            ll from = e.second.first;
+            ll to = e.second.second;
+            if(d[from] != INF && d[to] > d[from] + e.first){
+                d[to] = d[from] + e.first;
+                negNode[from] = negNode[to] = true;
+            }
+        }
+    }
 }
 
 
 /*==================================================*/
 int main(){
-  
-  //ここでグラフの生成
-  REP(i,0,N) d[i] = INF;
-  start = 0;
-  d[start] = 0;
-  bellman_ford();
+    //ここでグラフの生成
+    edge.clear();
+    REP(i, 0, N) d[i] = INF;
+    REP(i, 0, M) {
+        ll a, b, c;
+        cin >> a >> b >> c;
+        a--;
+        b--;
+        edge.push_back({-c, {a, b}});
+    }
+    start = 0;
+    d[start] = 0;
+    negCloCir = bellman_ford();
 
-  if(negCloCir){
-    //負の閉路がある場合の処理
-  }
-  
+    if (negCloCir) {
+        //負の閉路がある場合の処理
+        //例: 負の閉路によって更新されてしまうノードチェック(ABC061 D)
+        REP(i,0,N) negNode[i]=false;
+        check_negative();
+        if(negNode[N-1]){
+            p("inf");
+        }else{
+            p(-d[N - 1]);
+        }
+    } else {
+        p(-d[N - 1]);
+    }
+    return 0;
 }
+
